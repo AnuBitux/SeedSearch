80864e39d59c4a7ea0d80dd30e278860dc87a508
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2023-04-30 16:54:13 +0200

    Create seedsearch.sh

---
+++ seedsearch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo seedsearch.py is a python tool that scans all the files in a directory,
+echo including subdirectories, looking for BIP39 mnemonic seeds
+echo ''
+echo seedsearchpro.py is also available: if users agree, it will also try to derive addresses with the found seeds
+echo and check online if the addresses were used, trying to guess the right derivation path
+echo ''
+echo note that deriving lots of addresses could overload APIs
+echo users that have a blockcypher API key can provide it editing the proper line in the file in /Tools/Recovery/SeesSearch.py/seedsearchpro.py
+echo ''
+echo example: seedsearch.py -d /home/anubitux/Documents
+echo example: seedsearchpro.py -d /home/anubitux/Documents
+echo ''
+
+$SHELL
